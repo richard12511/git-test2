562916bf7b4c872daf8edb5a3d59a5eea075fcd0
Richard Schmidt <richardnew12511@gmail.com>
2015-08-31 20:47:41 -0500

    Added some functions

--- LearningFSharp/LearningFSharp/Program.fs
+++ LearningFSharp/LearningFSharp/Program.fs
@@ -1,3 +1,21 @@
 ﻿
 let main argv = 
     printfn "Hello World" 
+
+let add x y =
+    x + y
+
+
+let isLessThan100 number = 
+    if number < 100 then
+        printfn "Less than 100"
+    else
+        printfn "Not less than 100"
+
+let answer = add 42 43 |> isLessThan100
+
+let double x =
+    x * 2
+
+let addTwoAndDouble x =
+    double(add x 2)
