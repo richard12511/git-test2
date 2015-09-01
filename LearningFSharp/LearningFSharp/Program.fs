5dd3aa53f2337b8f2d3d1f37acb372cfb311cd21
Richard Schmidt <richardnew12511@gmail.com>
2015-08-31 20:05:14 -0500

    Removed comments

--- LearningFSharp/LearningFSharp/Program.fs
+++ LearningFSharp/LearningFSharp/Program.fs
@@ -1,7 +1,3 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
-
-[<EntryPoint>]
+﻿
 let main argv = 
     printfn "Hello World" 
-    0 // return an integer exit code
