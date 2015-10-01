5091ee4dccdc9449279644d60f428afd7d228147
Richard Schmidt <richardnew12511@gmail.com>
2015-09-30 21:07:15 -0500

    added a test

--- LearningFSharp/LearningFSharpTests/PieChartDrawerTests.fs
+++ LearningFSharp/LearningFSharpTests/PieChartDrawerTests.fs
@@ -5,13 +5,30 @@ open LearningFSharp
 open System.Drawing
 
 [<TestFixture>]
-type PieChartDrawerTests() =
+module PieChartDrawerTests =
 
     [<Test>]
-    member x.convertDataRow_GivenAComma1_ReturnsTupleAComma1() =
+    let ``When I call ConvertDataRow with "A, 1" I should get a tuple (A,1)`` () =
         let pieChartDrawer = new PieChartDrawer()
 
         let (resultTitle, resultValue) = pieChartDrawer.ConvertDataRow("A, 1")
 
         Assert.AreEqual(resultTitle, "A")
         Assert.AreEqual(resultValue, 1)
+
+    [<Test>]
+    let ``When I call ConvertDataRow with "A, 1, B, 2" I should get a tuple (A,1)`` () =
+        let pieChartDrawer = new PieChartDrawer()
+
+        let (resultTitle, resultValue) = pieChartDrawer.ConvertDataRow("A, 1, B, 2")
+
+        Assert.AreEqual(resultTitle, "A")
+        Assert.AreEqual(resultValue, 1)
+
+    [<Test]
+    let ``When I call ConvertDataRow with "A" I should get a failure exception`` () =
+        let pieChartDrawer = new PieChartDrawer()
+
+        let (resultTitle, resultValue) = pieChartDrawer.ConvertDataRow("A")
+
+        Assert.Fail("bad")
